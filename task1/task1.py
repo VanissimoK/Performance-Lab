c0b921279fce3aefe0e0bc95d720280db7acae34
VanissimoK <130978854+VanissimoK@users.noreply.github.com>
2025-09-01 12:34:26 +0300

    Create task1.py

---
+++ task1/task1.py
@@ -0,0 +1,26 @@
+from itertools import cycle
+
+def build_path(n, m):
+    lst = list(range(1, n + 1))
+    clst = cycle(lst)
+    
+    frst = next(clst)
+    path = []
+    current = frst
+    
+    while current != frst or len(path) == 0:
+        path.append(current)
+        for _ in range(m - 1):
+            current = next(clst)
+    
+    return path
+
+n1, m1 = map(int, input().split())
+path1 = build_path(n1, m1)
+
+n2, m2 = map(int, input().split())
+path2 = build_path(n2, m2)
+
+sumpath = path1 + path2
+for i in range(len(sumpath)):
+    print(sumpath[i], end='')
