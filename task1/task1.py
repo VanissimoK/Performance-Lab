8026e27e2fbdf3860b46f16e5f6bbc135a0f3734
VanissimoK <130978854+VanissimoK@users.noreply.github.com>
2025-09-03 12:08:10 +0300

    Update task1.py.  Ввод значений из аргументов командной строки

--- task1/task1.py
+++ task1/task1.py
@@ -1,9 +1,9 @@
+import sys
 from itertools import cycle
 
 def build_path(n, m):
     lst = list(range(1, n + 1))
     clst = cycle(lst)
-    
     frst = next(clst)
     path = []
     current = frst
@@ -12,15 +12,11 @@ def build_path(n, m):
         path.append(current)
         for _ in range(m - 1):
             current = next(clst)
-    
     return path
 
-n1, m1 = map(int, input().split())
+n1, m1, n2, m2 = map(int, sys.argv[1:5])
 path1 = build_path(n1, m1)
-
-n2, m2 = map(int, input().split())
 path2 = build_path(n2, m2)
-
 sumpath = path1 + path2
 for i in range(len(sumpath)):
     print(sumpath[i], end='')
